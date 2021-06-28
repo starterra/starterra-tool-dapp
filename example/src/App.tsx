b2730624f05d7274948991424df8b8b6a815f93e
Marta <marta.nocon@gmail..com>
2021-06-28 10:18:48 +0200

    use anchor/wallet provider setup for network to make read only mode working again

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,106 +1,109 @@
 import React from 'react'
 
-import { WalletProvider, Wallet, Tokens ,NetworkInfo } from 'starterra-tool-dapp'
+import {
+  WalletProvider,
+  Wallet,
+  Tokens,
+  NetworkInfo
+} from 'starterra-tool-dapp'
 
 const avaliableNetworks: Record<string, NetworkInfo> = {
-  mainnet: {
-    name: "mainnet",
-    chainID: "columbus-4",
-    lcd: "https://lcd.terra.dev",
+  0: {
+    name: 'testnet',
+    chainID: 'tequila-0004',
+    lcd: 'https://tequila-lcd.terra.dev'
   },
-  testnet: {
-    name: "testnet",
-    chainID: "tequila-0004",
-    lcd: "https://tequila-lcd.terra.dev",
-  },
+  1: {
+    name: 'mainnet',
+    chainID: 'columbus-4',
+    lcd: 'https://lcd.terra.dev'
+  }
 }
 
-const testnetTokens:Tokens= 
-   [{
+const testnetTokens: Tokens = [
+  {
     address: 'uluna',
     name: 'LUNA',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'uusd',
+    address: 'uusd',
     name: 'UST',
     isDefault: true,
     decimal: 6
   },
   {
-    address:'ukrw',
+    address: 'ukrw',
     name: 'KRW',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'usdr',
+    address: 'usdr',
     name: 'SDR',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'terra10llyp6v3j3her8u3ce66ragytu45kcmd9asj3u',
+    address: 'terra10llyp6v3j3her8u3ce66ragytu45kcmd9asj3u',
     name: 'MIR',
     isDefault: false,
     decimal: 6
   },
-   {
-    address:'terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc',
+  {
+    address: 'terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc',
     name: 'ANC',
     isDefault: false,
     decimal: 6
   }
 ]
 
-const mainnetTokens:Tokens= 
-   [{
+const mainnetTokens: Tokens = [
+  {
     address: 'uluna',
     name: 'LUNA',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'uusd',
+    address: 'uusd',
     name: 'UST',
     isDefault: true,
     decimal: 6
   },
   {
-    address:'ukrw',
+    address: 'ukrw',
     name: 'KRW',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'usdr',
+    address: 'usdr',
     name: 'SDR',
     isDefault: false,
     decimal: 6
   },
   {
-    address:'terra15gwkyepfc6xgca5t5zefzwy42uts8l2m4g40k6',
+    address: 'terra15gwkyepfc6xgca5t5zefzwy42uts8l2m4g40k6',
     name: 'MIR',
     isDefault: false,
     decimal: 6
   },
-   {
-    address:'terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76',
+  {
+    address: 'terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76',
     name: 'ANC',
     isDefault: false,
     decimal: 6
   }
 ]
 const App = () => {
-  
   return (
     <WalletProvider
-      defaultNetwork={avaliableNetworks['testnet']}
+      defaultNetwork={avaliableNetworks[0]}
       walletConnectChainIds={avaliableNetworks}
-
     >
-      <Wallet tokens={testnetTokens}/>
+      <Wallet tokens={testnetTokens} />
     </WalletProvider>
   )
 }
--- example/src/avaliableNetworks.ts
+++ example/src/avaliableNetworks.ts
@@ -4,12 +4,12 @@ import {
 
 
 export const avaliableNetworks: Record<string, NetworkInfo> = {
-  mainnet: {
+  0: {
     name: "mainnet",
     chainID: "columbus-4",
     lcd: "https://lcd.terra.dev",
   },
-  testnet: {
+  1: {
     name: "testnet",
     chainID: "tequila-0004",
     lcd: "https://tequila-lcd.terra.dev",
