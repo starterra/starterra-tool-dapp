5944990848a7fa614cecac9d46656c6a0ccfc249
Marta H <martanocon@gmail.com>
2021-07-04 11:15:06 +0200

    #26 added info about empty balance (#28)
    Co-authored-by: Marta <marta.nocon@gmail..com>

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { tokenValue } from '../utils'
 import { TokenBalance } from '../types/token'
 import useStyles from '../styles/useStyles'
+import * as trans from '../translation'
 
 interface BalanceProps {
   tokenBalance: TokenBalance[]
@@ -11,6 +12,9 @@ const Balance: FC<BalanceProps> = ( { tokenBalance } ) => {
   const classes = useStyles()
   return (
     <div className={classes.balanceSection}>
+      {tokenBalance.length ===0}{
+        <h4>{trans.EMPTY_WALLET}</h4>
+      }
       {tokenBalance.map((item) => (
         <div className={classes.balanceItem} key={item.name}>
           <h4>{item.name}</h4>
