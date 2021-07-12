2358f48e66ac8cbb555e102a3d5e04120929955c
Marta <marta.nocon@gmail..com>
2021-07-12 20:44:20 +0200

    #17 rafactor according to PR comment

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -68,44 +68,44 @@ const testnetTokens: Tokens = [
   }
 ]
 
-const mainnetTokens: Tokens = [
-  {
-    address: 'uluna',
-    name: 'LUNA',
-    isDefault: false,
-    decimal: 6
-  },
-  {
-    address: 'uusd',
-    name: 'UST',
-    isDefault: true,
-    decimal: 6
-  },
-  {
-    address: 'ukrw',
-    name: 'KRW',
-    isDefault: false,
-    decimal: 6
-  },
-  {
-    address: 'usdr',
-    name: 'SDR',
-    isDefault: false,
-    decimal: 6
-  },
-  {
-    address: 'terra15gwkyepfc6xgca5t5zefzwy42uts8l2m4g40k6',
-    name: 'MIR',
-    isDefault: false,
-    decimal: 6
-  },
-  {
-    address: 'terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76',
-    name: 'ANC',
-    isDefault: false,
-    decimal: 6
-  }
-]
+// const mainnetTokens: Tokens = [
+//   {
+//     address: 'uluna',
+//     name: 'LUNA',
+//     isDefault: false,
+//     decimal: 6
+//   },
+//   {
+//     address: 'uusd',
+//     name: 'UST',
+//     isDefault: true,
+//     decimal: 6
+//   },
+//   {
+//     address: 'ukrw',
+//     name: 'KRW',
+//     isDefault: false,
+//     decimal: 6
+//   },
+//   {
+//     address: 'usdr',
+//     name: 'SDR',
+//     isDefault: false,
+//     decimal: 6
+//   },
+//   {
+//     address: 'terra15gwkyepfc6xgca5t5zefzwy42uts8l2m4g40k6',
+//     name: 'MIR',
+//     isDefault: false,
+//     decimal: 6
+//   },
+//   {
+//     address: 'terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76',
+//     name: 'ANC',
+//     isDefault: false,
+//     decimal: 6
+//   }
+// ]
 const App = () => {
   return (
     <WalletProvider
