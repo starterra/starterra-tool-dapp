c6d2f96c0d8839cddf48cfbffda408a6918f4d67
Marta <marta.nocon@gmail..com>
2021-07-12 19:03:45 +0200

    #17 rafactor according to PR comment

--- src/components/SendDialog.tsx
+++ src/components/SendDialog.tsx
@@ -155,7 +155,7 @@ const SendDialog: FC<SendProps> = ({ wallletAddress, tokensBalance }) => {
             <DialogTitle id='form-dialog-title'>Status</DialogTitle>
 
             <DialogContent>
-              <div>{pending && <Spinner shouldRender={pending} />}</div>
+              <div><Spinner shouldRender={pending} /></div>
               <TransactionResult
                 response={response}
                 error={txError}
