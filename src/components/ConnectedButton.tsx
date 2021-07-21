be98db8c6605ef7411b2e2f59917440b05455f2e
Marta H <martanocon@gmail.com>
2021-07-21 13:07:12 +0200

    Update styles (#57)
    * #48 don't display balance for tokens not defined in token config
    
    * #27 add fee esitmation using terra js and update handling tx result
    
    * #57 update styles 
    
    
    Co-authored-by: Marta <marta.nocon@gmail..com>

--- src/components/ConnectedButton.tsx
+++ src/components/ConnectedButton.tsx
@@ -1,37 +1,94 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Button from '@material-ui/core/Button'
+import { withStyles, Theme } from '@material-ui/core'
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet'
 import { getEllipsisTxt } from '../utils'
 import { TokenBalance } from '../types/token'
 import { tokenValueTxt } from '../utils'
+import { useMediaQuery } from 'react-responsive'
+
+const Connected = withStyles((theme: Theme) => ({
+  root: {
+    textTransform: 'uppercase',
+    backgroundColor: theme.palette.info.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.info.main
+    }
+  }
+}))(Button)
+
+const ConnectedActive = withStyles((theme: Theme) => ({
+  root: {
+    textTransform: 'uppercase',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.info.main,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.info.main
+    }
+  }
+}))(Button)
 
 interface ConnectButtonProps {
   address: string
   defaultToken: TokenBalance
+  open: boolean
   onClick?: () => void
 }
 
 const ConnectedButton: FC<ConnectButtonProps> = ({
   address,
   onClick,
-  defaultToken
+  defaultToken,
+  open
 }) => {
-  return (
-    <Button
-      variant='contained'
+  const isMobile = useMediaQuery({ maxWidth: 850 })
+  const [hover, setHover] = useState(false)
+  return open ? (
+    <ConnectedActive
+      variant='outlined'
       color='primary'
       onClick={onClick}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
       data-testid='connected-button'
-      className={'wallet-connect-button'}
-      startIcon={<AccountBalanceWalletIcon />}
+      className='wallet-connect-button'
+      startIcon={<AccountBalanceWalletIcon style={{ fontSize: 15 }} />}
     >
-      <span>{getEllipsisTxt(address)}</span>
+      {!isMobile && <span>{getEllipsisTxt(address)}</span>}
       {defaultToken && (
-        <span className={'wallet-balance-button'}>
+        <span className='wallet-balance-button'>
           {tokenValueTxt(defaultToken)}
         </span>
       )}
-    </Button>
+    </ConnectedActive>
+  ) : (
+    <Connected
+      variant='outlined'
+      color='primary'
+      onClick={onClick}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      data-testid='connected-button'
+      className='wallet-connect-button'
+      startIcon={<AccountBalanceWalletIcon style={{ fontSize: 15 }} />}
+    >
+      {!isMobile && hover ? (
+        <span>{getEllipsisTxt(address)}</span>
+      ) : (
+        !isMobile && (
+          <span className='wallet-connect-addess'>
+            {getEllipsisTxt(address)}
+          </span>
+        )
+      )}
+      {defaultToken && (
+        <span className='wallet-balance-button'>
+          {tokenValueTxt(defaultToken)}
+        </span>
+      )}
+    </Connected>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,72 +1,129 @@
+import './styles.css'
+
 import * as React from 'react'
-import ConnectWallet, { IConnectWalletProps } from './ConnectWallet'
+
 import {
-  TokenBalance as TokenBalanceIm,
-  Tokens as TokensIm
-} from './types/token'
-import {
-  WalletStatus,
   ConnectType,
-  WalletInfo as WalletInfoIm,
-  WalletControllerOptions as WalletControllerOptionsIm,
-  WalletController,
-  StringifiedTxResult as StringifiedTxResultIm,
-  findTxResult,
-  WalletProvider,
   ExtensionNetworkOnlyWalletProvider,
-  useWallet,
+  StringifiedTxResult as StringifiedTxResultIm,
+  WalletController,
+  WalletControllerOptions as WalletControllerOptionsIm,
+  WalletInfo as WalletInfoIm,
+  WalletProvider,
+  WalletStatus,
+  findTxResult,
   useConnectedWallet,
-  useInstallChromeExtension
+  useInstallChromeExtension,
+  useWallet
 } from '@terra-money/wallet-provider'
+import ConnectWallet from './ConnectWallet'
 import {
   NetworkInfo as NetworkInfoIm,
   TxResult as TxResultIm
 } from '@terra-dev/wallet-types'
+import { Theme } from '@material-ui/core'
+
+import {
+  TokenBalance as TokenBalanceIm,
+  Tokens as TokensIm
+} from './types/token'
+
 import { ThemeProvider } from '@material-ui/core/styles'
 import { createMuiTheme } from '@material-ui/core/styles'
-import './styles.css'
 
-const theme = createMuiTheme({
+const globalTheme = createMuiTheme({
   palette: {
     primary: {
       main: '#2043b5'
     },
     secondary: {
       main: '#0044ff'
-    }
-  },
-  typography: {
-    fontFamily: ['"Gotham"', 'sans-serif', '-apple-system'].join(',')
-  },
-  overrides: {
-    MuiPaper: {
-      root: {
-        color: '#2043b5'
-      }
     },
-    MuiButton: {
-      containedPrimary: {
-        height: '30px',
-        borderRadius: '20px',
-        fontSize: '12px'
-      }
-    },
-    MuiInputBase: {
-      input: {
-        color: '#2043b5',
-        border: '1px solid rgba(12,54,148,.2)'
-      }
+    info: {
+      main: '#e8eaed',
+      dark: '#ffffff'
     }
   }
 })
 
-export const Wallet = ({ tokens, readOnlyMode }: IConnectWalletProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <ConnectWallet tokens={tokens} readOnlyMode={readOnlyMode} />
-    </ThemeProvider>
-  )
+const theme = createMuiTheme(
+  {
+    overrides: {
+      MuiPaper: {
+        root: {
+          color: globalTheme.palette.secondary.main,
+          backgroundColor: globalTheme.palette.info.dark,
+          border: `3px solid ${globalTheme.palette.primary.main}`
+        },
+        rounded: {
+          borderRadius: '20px'
+        }
+      },
+      MuiButton: {
+        contained: {
+          '&.Mui-disabled': {
+            color: globalTheme.palette.primary.main,
+            opacity: 0.5
+          }
+        },
+        containedPrimary: {
+          height: '34px',
+          borderRadius: '21px',
+          fontSize: '13px',
+          textTransform: 'initial',
+          fontWeight: 'bold',
+          '&:hover': {
+            backgroundColor: globalTheme.palette.primary.main
+          }
+        },
+        outlinedPrimary: {
+          border: `4px solid ${globalTheme.palette.primary.main}`,
+          borderRadius: '21px',
+          '&:hover': {
+            border: `4px solid ${globalTheme.palette.primary.main}`
+          }
+        },
+        outlinedSecondary: {
+          borderRadius: '21px',
+          marginBottom: '15px',
+          backgroundColor: globalTheme.palette.info.main,
+          textTransform: 'initial',
+          border: 'none',
+          '&:hover': {
+            border: 'none',
+            backgroundColor: globalTheme.palette.primary.main,
+            color: globalTheme.palette.info.main
+          }
+        }
+      },
+
+      MuiInputBase: {
+        input: {
+          backgroundColor: globalTheme.palette.info.dark,
+          color: globalTheme.palette.secondary.main,
+          border: `3px solid ${globalTheme.palette.primary.main}`
+        }
+      },
+      MuiFormLabel: {
+        root: {
+          color: globalTheme.palette.secondary.main
+        }
+      }
+    }
+  },
+  globalTheme
+)
+
+export interface IWalletProps {
+  tokens: Tokens
+  readOnlyMode: boolean
+  customTheme?: Theme
 }
+export const Wallet = ({ tokens, readOnlyMode, customTheme }: IWalletProps) => (
+  <ThemeProvider theme={customTheme || theme}>
+    <ConnectWallet tokens={tokens} readOnlyMode={readOnlyMode} />
+  </ThemeProvider>
+)
 
 export type WalletInfo = WalletInfoIm
 export type WalletControllerOptions = WalletControllerOptionsIm
